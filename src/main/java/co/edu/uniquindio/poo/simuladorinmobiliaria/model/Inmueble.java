48d6ee0335d0565758c52f04ac45fcd12bce8493
Sofia <Sofa.suarezb@uqvirtual.Edu.co>
2026-05-23 16:26:48 -0500

    Conclusion estructura sin metodos

--- src/main/java/co/edu/uniquindio/poo/simuladorinmobiliaria/model/Inmueble.java
+++ src/main/java/co/edu/uniquindio/poo/simuladorinmobiliaria/model/Inmueble.java
@@ -1,4 +1,45 @@
 package co.edu.uniquindio.poo.simuladorinmobiliaria.model;
 
+import co.edu.uniquindio.poo.simuladorinmobiliaria.model.Enum.EstadoInmueble;
+import co.edu.uniquindio.poo.simuladorinmobiliaria.model.Enum.TipoInmueble;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Inmueble {
+    //Atributos
+    private String codigo;
+    private String direccion;
+    private String ciudad;
+    private double area;
+    private double precio;
+    private String descripcion;
+    private LocalDate fechaPublicacion;
+    private TipoInmueble tipoInmueble;
+    private EstadoInmueble estado;
+
+
+    //Relaciones
+    private Vendedor vendedorAsignado;
+    private List<Oferta> listaOfertas;
+    private Publicacion publicacion;
+    private GestorInmuebles ownedByGestorInmuebles;
+
+
+    public Inmueble(String codigo, String direccion, String ciudad, double area,
+                    double precio, String descripcion, TipoInmueble tipoInmueble) {
+        this.codigo = codigo;
+        this.direccion = direccion;
+        this.ciudad = ciudad;
+        this.area = area;
+        this.precio = precio;
+        this.descripcion = descripcion;
+        this.tipoInmueble = tipoInmueble;
+        this.estado = EstadoInmueble.DISPONIBLE;
+        this.fechaPublicacion = LocalDate.now();
+        this.listaOfertas = new ArrayList<>();
+    }
+
+
 }
--- src/main/java/co/edu/uniquindio/poo/simuladorinmobiliaria/model/Transaccion.java
+++ src/main/java/co/edu/uniquindio/poo/simuladorinmobiliaria/model/Transaccion.java
@@ -1,4 +1,8 @@
 package co.edu.uniquindio.poo.simuladorinmobiliaria.model;
 
-public record Transaccion() {
+import co.edu.uniquindio.poo.simuladorinmobiliaria.model.Enum.TipoOperacion;
+
+import java.time.LocalDate;
+
+public record Transaccion(String codigo, Comprador comprador, Vendedor vendedor, Inmueble inmueble, double valorFinal, TipoOperacion tipoOperacion, LocalDate fecha, GestorTransacciones ownedByTransacciones) {
 }
