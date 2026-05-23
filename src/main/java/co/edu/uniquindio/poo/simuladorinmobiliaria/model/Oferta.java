acb679b3c0f67348355eb3e74756ac2048082a3a
Sofia <Sofa.suarezb@uqvirtual.Edu.co>
2026-05-23 16:26:48 -0500

    Conclusion estructura sin metodos

--- src/main/java/co/edu/uniquindio/poo/simuladorinmobiliaria/model/Oferta.java
+++ src/main/java/co/edu/uniquindio/poo/simuladorinmobiliaria/model/Oferta.java
@@ -1,4 +1,31 @@
 package co.edu.uniquindio.poo.simuladorinmobiliaria.model;
 
+import co.edu.uniquindio.poo.simuladorinmobiliaria.model.Enum.EstadoOferta;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+@Getter
+@Setter
 public class Oferta {
+    //Atributos
+    private String codigo;
+    private double valor;
+    private LocalDate fecha;
+    private EstadoOferta estadoOferta;
+
+    //Relaciones
+
+    private Inmueble ownedByInmueble;
+    private Comprador comprador;
+
+    public Oferta(String codigo, double valor,  Comprador comprador, Inmueble ownedByInmueble) {
+        this.codigo = codigo;
+        this.valor = valor;
+        this.fecha = LocalDate.now();
+        this.estadoOferta = EstadoOferta.PENDIENTE;
+        this.ownedByInmueble= ownedByInmueble;
+        this.comprador= comprador;
+
+    }
 }
--- src/main/java/co/edu/uniquindio/poo/simuladorinmobiliaria/model/Publicacion.java
+++ src/main/java/co/edu/uniquindio/poo/simuladorinmobiliaria/model/Publicacion.java
@@ -1,4 +1,29 @@
 package co.edu.uniquindio.poo.simuladorinmobiliaria.model;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 public class Publicacion {
+
+    //Atributos
+    private String codigo;
+    private LocalDate fecha;
+    private LocalTime hora;
+    private String descripcion;
+
+    //Relaciones
+    private Vendedor vendedor;
+    private Inmueble inmueble;
+    private GestorPublicacion gestorPublicacion;
+
+
+    public Publicacion(String codigo, String descripcion, Vendedor vendedor, Inmueble inmueble) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.vendedor = vendedor;
+        this.inmueble = inmueble;
+        this. hora= LocalTime.now();
+        this.fecha=LocalDate.now();
+    }
 }
